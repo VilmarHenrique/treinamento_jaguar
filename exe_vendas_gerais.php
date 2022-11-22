03e09075e8e3c68d05a0217c3f8a1eafa23b6263
Vilmar Henrique <vilmarh.andrade@gmail.com>
2022-11-22 15:19:47 -0300

    add jaguar and php

---
+++ exe_vendas_gerais.php
@@ -0,0 +1,83 @@
+<?php
+  session_start();
+  require_once("lib/jaguar/jaguar.inc.php");
+  require_once("include/funcoes.inc.php");
+
+  //Html
+  $title = "Vendas Gerais";
+  $html = new JHtml($title);
+  $html->AddHtml("<h3>$title</h3>");
+ 
+  $conn->SetDebug(0);
+
+  //Form
+  $form = new JForm();
+
+  $label = "Arquivo";
+  $form->OpenRow();
+  $form->OpenHeader("Arquivo");
+  $form->OpenCell();
+  $ds_arquivo = new JFormFile("f_ds_arquivo");
+  $ds_arquivo->SetTestIfEmpty(true, "Preencha o campo {$label}!");
+  $form->AddObject($ds_arquivo);
+
+  $form->OpenRow();
+  $form->OpenHeader("", ["colspan" => 2]);
+  $submit = new JFormSubmit("f_submit", "Enviar");
+  $form->AddObject($submit);
+
+  if ($form->IsSubmitted())
+  {
+    $arr_arquivo = file($_FILES["f_ds_arquivo"]["tmp_name"]);
+   
+      $tbl = new JTable();
+
+      $tbl->OpenRow();
+      $tbl->OpenHeader("NF");
+      $tbl->OpenHeader("Cliente");
+      $tbl->OpenHeader("CNPJ");
+      $tbl->OpenHeader("Produto");
+      $tbl->OpenHeader("Valor");
+      $tbl->OpenHeader("Qtd");
+
+      foreach ($arr_arquivo as $ds_linha)
+      {                      
+        
+          $arr_linha = explode(";", $ds_linha);
+
+          $nr_nota_fiscal = $arr_linha[0];
+          $cd_pessoa      = $arr_linha[1];
+          $nm_pessoa      = $arr_linha[2];
+          $nr_cnpj        = $arr_linha[3];
+          $cd_produto     = $arr_linha[7]; 
+          $nm_produto     = $arr_linha[8];
+          $vl_financeiro  = $arr_linha[9];
+          $qt_unid_cx     = Format_Number($arr_linha[10], 2, "pt_BR", "sys");
+          $vl_volume_cx   = Format_Number($arr_linha[11], 2, "pt_BR", "sys");
+
+          $soma = $qt_unid_cx * $vl_volume_cx;
+
+          if (!is_numeric($nr_nota_fiscal)){
+          
+            continue;
+          }
+                             
+          $tbl->OpenRow();
+          $tbl->OpenCell($nr_nota_fiscal);     
+          $tbl->OpenCell($nm_pessoa);       
+          $tbl->OpenCell($nr_cnpj);        
+          $tbl->OpenCell($nm_produto);       
+          $tbl->OpenCell($vl_financeiro);      
+          $tbl->OpenCell($soma);          
+      }
+    }  
+    
+       
+  $html->AddObject($form);
+
+
+  $html->AddObject($tbl);
+
+  echo $html->GetHtml();
+
+  $conn->Close();
