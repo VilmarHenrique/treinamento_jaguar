75b635a42ee48d181310eda2e2bddd3e6e24a06a
Vilmar Henrique <vilmarh.andrade@gmail.com>
2022-11-23 14:38:03 -0300

    tarefas

---
+++ desafio.php
@@ -0,0 +1,47 @@
+<?php
+    $arr_numeros = [];
+    
+  //Leio os registros e armazeno no array "arr_numeros"
+  for ($i = 1; $i <= 2; $i++)
+  {
+    $arr_numeros = readline("Informe 10 numeros:");
+
+
+  }
+
+
+
+
+/*     if($arr_numeros % 2 == 0)
+    { echo "Numero Par"; }
+     else {
+       echo "Numero Impar"; 
+      }
+       
+    if($arr_numeros < 0){
+      echo "Negativo";}
+    else{
+      echo "Positivo";
+    }
+  }
+  
+
+ */
+/* 
+  Entre com 10 números e armazene em um array.
+  Ao final o programa deverá mostrar:
+  - quantos negativos foram digitados;
+  - quantos positivos foram digitados;
+  - quantos pares e ímpares.
+ */
+
+
+
+
+
+
+
+
+
+
+?>
